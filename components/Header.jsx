e3d8fcbc6ca280b8cf43946897227e02b7fc8d8b
dunghq21102001 <dunghq21102001@gmail.com>
2022-10-10 00:49:22 +0700

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+const Header = () => {
+    return (
+        <header className=''>
+            <Image className='object-contain' width={150} height={150} src='https://img.pikbest.com/png-images/20211018/colorful-alphabet-d-with-mandala-design_6151614.png!bw700' alt=''/>
+        </header>
+    )
+}
+
+export default Header
--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'image.tmdb.org', 'img.pikbest.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
